65b1794cc9dcb0606f1ed5b946141bfe4d75ba9c
chingtong <chingtong@chingtongs-MacBook-Air.local>
2017-10-26 18:02:45 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -21,25 +21,66 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
+var score = 0;
+var scoringSystem = document.getElementById('scoring');
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		score += 1;
 	} else {
 		alert("Sorry, try again.");
 	}
 };
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-
-if (cardsInPlay.length === 2) {
-	checkForMatch();
-} 
+var checkForSecondMatch = function () {
+	if (cardsInPlay[2] === cardsInPlay[3]) {
+		alert("You found a match!");
+		score += 1;
+	} else {
+		alert("Sorry, try again.");
+	}
 };
 
-flipCard(0);
-flipCard(2);
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	} else if (cardsInPlay.length === 4) {
+		checkForSecondMatch();
+	}
+
+	scoringSystem.textContent = 'Current score: ' + score;
+};
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+scoringSystem.textContent = 'Current score: ' + score;
+
+// reset button
+var reset = function () {
+	var gameNode = document.getElementById('game-board');
+	for (var i = 0; i < cards.length; i++) {
+		gameNode.removeChild(gameNode.firstChild);
+	}
+	createBoard();
+	cardsInPlay = [];
+	score = 0;
+	scoringSystem.textContent = 'Current score: ' + score;
+};
+var button = document.getElementsByTagName('button')[0];
+button.addEventListener('click', reset);
